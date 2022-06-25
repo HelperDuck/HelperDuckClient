4bbd692162cab1e66dd4f33fe2c68f28ba2b1dc6
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-25 15:43:45 +0200

    FE: PAGES: skeleton

---
+++ src/Pages/CreateProfilePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+  
+}
+
+export const CreateProfilePage = (props: Props)  =>{
+  return (
+    
+    <div>
+      
+    </div>
+  );
+}
---
+++ src/Pages/ProfilePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+  
+}
+
+export const ProfilePage = (props: Props)  =>{
+  return (
+    
+    <div>
+      
+    </div>
+  );
+}
