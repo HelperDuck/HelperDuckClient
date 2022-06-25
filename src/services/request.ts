38c46574a6440cb3c6c4a44c8523e463e453eb4c
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-25 11:55:45 +0200

    FE:SERVICES:chore: try/catch && async/await for postRequest and getAllIncomingRequests

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,13 +1,26 @@
 import { requestAskedType } from "../Types/RequestAskedType";
 import { UserType } from "../Types/UserType";
 
-export async function postRequest( user: UserType, request: requestAskedType) {
-  console.log(user, 'user');
-  console.log(request, 'request');
+const BASE_URL = 'http://localhost:3002'
+
+export async function postRequest( request: requestAskedType) {
+  try {
+  const newRequest: any = await fetch(`${BASE_URL}/post`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(request),
+  });
+  return await newRequest;
+  } catch (err) {
+    console.log('Error at postRequest Service: ', err)
+  }
 }
 
-export async function getAllIncomingRequests( user: UserType, request: requestAskedType) {
-  console.log(user, 'user');
-  console.log(request, 'request');
+export async function getAllIncomingRequests(): Promise<any> {
+  try {
+    const allIncomingRequests = await fetch(`${BASE_URL}/get`);
+    return await allIncomingRequests.json();
+  } catch (err) {
+      console.log('Error at getAllIncomingRequests Service: ', err);
+  }
 }
-
