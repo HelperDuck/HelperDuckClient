94e194a2fb9aa18fc1db03f7c9ad2ed4d01e3b35
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-25 11:30:26 +0200

    FE:SERVICES: rename service and initialize getAllIncomingRequests

--- src/services/request.ts
+++ src/services/request.ts
@@ -5,3 +5,9 @@ export async function postRequest( user: UserType, request: requestAskedType) {
   console.log(user, 'user');
   console.log(request, 'request');
 }
+
+export async function getAllIncomingRequests( user: UserType, request: requestAskedType) {
+  console.log(user, 'user');
+  console.log(request, 'request');
+}
+
