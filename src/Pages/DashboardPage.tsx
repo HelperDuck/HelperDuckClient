34b709eebebfaaf2b7993f249125a477099c3b27
Mauricio Scain <scainmauricio@gmail.com>
2022-06-27 15:47:37 +0200

    DashUserInfo component

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { DashUserInfo } from '../components/DashUserInfo';
 import { IncomingRequest } from '../components/IncomingRequest';
 import { ProfilePerformanceInfo } from '../components/ProfilePerformanceInfo';
 import { RequestHistory } from '../components/RequestHistory';
-
+import boySvg from '../media/boy.svg'
 type Props = {
   
 }
@@ -11,9 +12,11 @@ export const DashboardPage = (props: Props)  =>{
   return (
     
     <div>
+       <div className='about-me-icon'><img src={boySvg} alt="boy img" />  </div>
       <IncomingRequest></IncomingRequest>
       <ProfilePerformanceInfo></ProfilePerformanceInfo>
       <RequestHistory></RequestHistory>
+      <DashUserInfo></DashUserInfo>
     </div>
   );
 }
--- src/components/DashUserInfo.tsx
+++ src/components/DashUserInfo.tsx
@@ -1,14 +1,65 @@
+import { Icon } from '@iconify/react';
 import React from 'react';
-
+import brainBook from '../media/brainBook.svg'
+import './DashUserInfo.css'
 type Props = {
-  
+
 }
 
-export const DashUserInfo = (props: Props)  =>{
-  return (
-    
-    <div>
-      
+export const DashUserInfo = (props: Props) =>{
+return (
+
+<div className='dash-user-wrapper'>
+  <div className='credits-taken-outer-container'>
+    <div className='credits-container'>
+      <div className='number'><span>55</span></div>
+      <div className='word'><span>Credits</span> </div>
     </div>
-  );
+    <div className='taken-container'>
+      <div className='number'><span>55</span></div>
+      <div className='word'><span>Taken requests</span> </div>
+    </div>
+  </div>
+  <div className='languages-outer-container'>
+    <div className='languages-inner-container'>
+      <div className='languages-icon'>
+        <Icon className='icon1' icon="lucide:languages" />
+      </div>
+      <div className='languages-container'>
+        <div className='languages-title'><span>Languages</span></div>
+        <div className='languages-list'>
+          <div>Portuguese</div>
+          <div>English</div>
+        </div>
+      </div>
+    </div>
+
+
+    <div className='expertise-inner-container'>
+      <div className='languages-icon'>
+        <Icon className='icon1' icon="healthicons:eyeglasses" />
+      </div>
+      <div className='languages-container'>
+        <div className='languages-title'><span>Expertise</span></div>
+        <div className='languages-list'>
+          <div className='test'>
+            <div className='singleLang'>react</div>
+            <div className='singleLang'>redux</div>
+            <div className='singleLang'>JavaScript</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className='aboutme-outer-container'>
+    <div className='about-me-container'>
+      <div className='about-me-title'>About me</div>
+      <div className='about-me-description'>+10 years of
+        experience.....blablablablablablablablablablablablablablablablablablablablablablablablablablabla</div>
+    </div>
+    <div className='about-me-icon'><img src={brainBook} alt="boy img" />
+    </div>
+  </div>
+</div>
+);
 }
