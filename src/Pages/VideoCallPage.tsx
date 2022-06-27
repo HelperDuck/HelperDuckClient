6db2e3ad88bf85cd553e611f2a6da15f50692d29
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-27 18:43:41 +0200

    FE:VideoCallPage - Video component + useEffect

--- src/Pages/VideoCallPage.tsx
+++ src/Pages/VideoCallPage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import Peer from 'simple-peer';
+import Peer, { Instance, SimplePeer } from 'simple-peer';
 import { Socket } from 'socket.io-client';
 import { io } from "socket.io-client";
 import socketIOClient from 'socket.io-client';
@@ -8,8 +8,35 @@ import { WebRTCUser } from '../Types/WebRTCUser'
 // console.log(socketIO);
 
 
-const URL = 'http://localhost:3002/';
+const SOCKET_SERVER_URL = 'http://localhost:3002/';
 
+type VideoProps = {
+  stream?: MediaStream
+  peer: Instance ; 
+}
+
+const Video = (props: VideoProps) => {
+  const ref = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    //@ts-ignore
+    props.peer.on('stream', (stream) => {
+      if(ref.current) ref.current.srcObject = stream;
+    });
+  }, []);
+
+  return (
+    <>
+      <video
+        playsInline
+        muted
+        autoPlay
+        ref={ref}
+        className="video-container"
+      />
+    </>
+  );
+};
 
 
 type Props = {
@@ -27,18 +54,20 @@ export const VideoCallPage = (props: Props)  =>{
     
     useEffect(() => {
       //@ts-ignore
-      const testSocket = socketIOClient.connect(URL);
-      // const socket = io(URL);
-      console.log(testSocket, 'success on client side')
+      // const testSocket = socketIOClient.connect(SOCKET_SERVER_URL);
+      const socket = io(SOCKET_SERVER_URL);
+      console.log(socket, 'success on client side')
     },[]);
     
 
   
-  
+  //TODO: line 69 will most likely be changed, but it compiles for now
   return (
     
     <div>
       "Hello World"
+      <Video peer={new Peer()} /> 
     </div>
   );
 }
+
