5faaff7e0b47719268f4d2c9a34edcc4ed25ad3f
noelcv <79803356+noelcv@users.noreply.github.com>
2022-07-05 18:43:32 +0200

    API

--- src/components/payments/Layout.tsx
+++ src/components/payments/Layout.tsx
@@ -4,7 +4,8 @@ import {ReactNode} from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
-const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY as string);
+//PUBLIC KEY 
+const stripePromise = loadStripe('pk_test_51LHZS2FoVOc20WxaO5cuf75NsH25K6llpma2IGQeL41UgJxMpyayAPYCULNjih58c49F8eRkV574TjOG49lNOweY004d2hRTmt');
 
 type Props = {
   children?: ReactNode;
