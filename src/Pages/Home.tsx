69ea1f625e2a18b15afc5e073bf9e7f6f2b80289
fegananca <fegananca@gmail.com>
2022-07-06 14:59:16 +0200

    test errors

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
