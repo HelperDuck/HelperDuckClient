31301f57ba166d109aa16428487cd9e85a388e96
Mauricio Scain <scainmauricio@gmail.com>
2022-07-06 15:54:36 +0200

    typescript prob

--- src/components/dashboard/SinglePastRequest.tsx
+++ src/components/dashboard/SinglePastRequest.tsx
@@ -1,7 +1,6 @@
 import { Icon } from "@iconify/react";
 import React from "react";
 import { useSelector } from "react-redux";
-import DetailsModal from "../DetailsModal";
 import "./SinglePastRequest.css";
 
 type Props = {
